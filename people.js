4f2b103192648093704b7bd4f21a804ea05d0f81
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-06 14:31:47 +0100

    forse ?

--- people.js
+++ people.js
@@ -1,2 +1,9 @@
 const hobbies = require("./hobbies");
 const fullName = require("./names");
+
+function people() {
+  fullName = ("Vincenzo", "Amari");
+  hobbies = ("Videogiochi", "Palestra", "Musica");
+
+  return { fullName, hobbies };
+}
